e67965a695768ea4556ccea8dbc0e63401c70268
Mvb1122 <micahbushman.school@gmail.com>
2024-05-13 15:26:43 -0600

    Image for 8Ball

--- 8ball.js
+++ 8ball.js
@@ -1,19 +1,73 @@
 //Ignore ts(80001)
-const { SlashCommandBuilder, CommandInteraction } = require('discord.js');
+const { SlashCommandBuilder, CommandInteraction, Message } = require('discord.js');
+const fs = require('fs')
 
 const pos = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"];
 const nc = ["Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
 const neg = ["Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
 const all = [pos, nc, neg];
 
-let pipe;
+let pipe, BallBuffer;
 // Loads the AI in advance to decrease time-to-response for user.
 async function Preload() {
     // Allocate a pipeline for sentiment-analysis
     const { pipeline } = await import("@xenova/transformers");
     pipe = await pipeline('sentiment-analysis', "Xenova/distilbert-base-uncased-finetuned-sst-2-english");
+
+    // Preloading the ball image makes the image generation take about 1ms as opposed to like 15ms.
+    fs.readFile("./8ball.png", (e, d) => {
+        BallBuffer = d;
+    })
 }
 Preload();
+
+// Use sharp to make an image.
+const sharp = require('sharp')
+/**
+ * @param {String} Text Input text to go on the ball.
+ * @param {String} path Path to save to.
+ * @returns {Promise<String>} Promise that resolves to the path when image is saved.
+ */
+async function MakeImage(Text, path) {    
+    // Break text into tspans:
+    const lines = Text.split(" ");
+    Text = ""
+    for (let i = 0; i < lines.length; i++) {
+        Text += `<tspan x="0" dy="1.2em" dx="50%">${lines[i]}</tspan>`;
+    }
+    
+    // Use canvas to make the 8ball image.
+    const img = sharp(BallBuffer);
+
+    // Create text svg.
+    const svg = `
+    <svg width="500" height="500">
+        <style>
+        .title { 
+            fill: #5BCEFA; 
+            font-size: 80px; 
+            font-weight: bold; 
+            font-family: "sans serif";
+            white-space: break-spaces;
+        }
+        </style>
+        <text y="0" text-anchor="middle" class="title">${Text}</text>
+    </svg>`
+    const SVGBuffer = Buffer.from(svg)
+
+    img.composite([{ 
+        input: SVGBuffer,
+    }]);
+
+    return new Promise((res, rej) => {
+        img.toFile(path, (e) => {
+            if (e) {
+                console.log(e);
+                rej(e);
+            } else res(path);
+        })
+    })
+}
 
 /**
  * Gets the emotion associated with a chunk of text. 
@@ -69,6 +123,38 @@ module.exports = {
             answer = sentiment[Math.floor(Math.random() * sentiment.length)];
         }
 
-        return interaction.editReply(`${QuestionStarter}My prediction is... **${answer}**`);
+        // Get image.
+        const path = `./Temp/${interaction.user.id}_ball.png`;
+        MakeImage(answer, path).then(() => {
+            // Save to temp folder and then send it off.
+            interaction.editReply({content: `${QuestionStarter}My prediction is... **${answer}**!`, files: [path]}).then(() => {
+                // Delete it.
+                fs.unlink(path, (err) => {if (err) console.log(err)});
+            })
+        })
     },
+
+    /**
+     * Executes code when message is recieved.
+     * @param {Message} message The inputted message. 
+     */
+    async OnMessageRecieved(message) {
+        // Block all old 8ball requests.
+        if (message.content.startsWith("--8ball") && !message.author.bot) {
+            // Forward 8ball requests by spoofing interaction things.
+            message.deferReply = () => { ; } // Empty function. 
+            message.editReply = message.reply;
+            message.user = message.author;
+            message.options = {};
+            message.options.getString = (s) => { message.content.substring(7) }
+
+            this.execute(message);
+        } 
+        
+        // Block Yggdrasil's responses.
+        else if (message.content.startsWith("🎱") && message.author.username == "Yggdrasil")
+        {
+            message.delete();
+        }
+    }
 };
