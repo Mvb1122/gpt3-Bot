b77a34d998e4efb06ee399b3c68232480a2d0869
Mvb1122 <micahbushman.school@gmail.com>
2025-08-02 09:40:01 -0600

    Add additional Caption modes for captioning function.

--- Commands/Caption.js
+++ Commands/Caption.js
@@ -36,8 +36,11 @@ module.exports = {
                     { name: "Detailed Caption", value: "<DETAILED_CAPTION>" },
                     { name: "Most Detailed Caption", value: "<MORE_DETAILED_CAPTION>" },
                     { name: "OCR / Read Text", value: "<OCR>" },
+                    { name: "OCR with Region", value: "<OCR_WITH_REGION>" },
                     { name: "Japanese OCR", value: "<MANGA_OCR>"},
                     { name: "Object Detection", value: "<OD>" },
+                    { name: "Dense Region Detection", value: "<DENSE_REGION_CAPTION>"},
+                    { name: "Region Proposal", "value": "REGION_PROPOSAL"}
                 ]);
         }),
 
@@ -60,7 +63,16 @@ module.exports = {
         const mode = interaction.options.getString("mode") ?? "<CAPTION>";
         let caption = await Caption(path, mode);
 
-        await interaction.editReply({content: "Caption: ```" + JSON.stringify(caption) + "```", files: [caption.image ? caption.image : path]});
+        let captionString = JSON.stringify(caption);
+        if (captionString.length < 2000)
+            await interaction.editReply({content: "Caption: ```" + captionString + "```", files: [caption.image ? caption.image : path]});
+        else {
+            // Write to a file. 
+            const captionPath = "./Temp/file_" + interaction.user.id + ".txt";
+            await fp.writeFile(captionPath, captionString);
+            await interaction.editReply({content: "Caption in below file.", files: [caption.image ? caption.image : path, captionPath]});
+            fp.unlink(captionPath)
+        }
         
         if (caption.image) fp.unlink(caption.image);
     }
